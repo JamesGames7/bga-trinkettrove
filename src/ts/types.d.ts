ad8b36dd927fa115cc3de153fb1f147790dcd136
Connor Rask <connor@srask.ca>
2026-07-22 08:08:54 -0400

    Typescript and scss setup

---
+++ src/ts/types.d.ts
@@ -0,0 +1,18 @@
+interface TrinketTroveTestPlayer extends Player {
+    energy: number; // any information you add on each result['players']
+}
+
+interface TrinketTroveTestGamedatas extends Gamedatas<TrinketTroveTestPlayer> {
+    // Add here variables you set up in getAllDatas
+}
+   
+/*
+ * Describe here the types for your state args
+ */
+interface PlayerTurnArgs {
+    playableCardsIds: number[];
+}
+   
+/*
+ * Describe here the types for your notif args
+ */
